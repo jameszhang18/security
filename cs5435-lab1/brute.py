5fe53a5d40c44ce94ef75e71790c09ffd7e808d9
jameszhang18 <zhangg18@outlook.com>
2019-09-13 16:32:55 -0400

    s

---
+++ cs5435-lab1/brute.py
@@ -0,0 +1,26 @@
+from csv import reader
+
+COMMON_PASSWORDS_PATH = 'common_passwords.txt'
+SALTED_BREACH_PATH = "app/scripts/breaches/salted_breach.csv"
+
+def load_breach(fp):
+    with open(fp) as f:
+        r = reader(f, delimiter=' ')
+        header = next(r)
+        assert(header[0] == 'username')
+        return list(r)
+
+def load_common_passwords():
+    with open(COMMON_PASSWORDS_PATH) as f:
+        pws = list(reader(f))
+    return pws
+
+def brute_force_attack(target_hash, target_salt):
+    pass
+
+def main():
+    salted_creds = load_breach(SALTED_BREACH_PATH)
+    brute_force_attack(salted_creds[0][1], salted_creds[0][2])
+
+if __name__ == "__main__":
+    main()
